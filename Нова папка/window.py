bfa2683b8f7cb61bc05f77e3d90cc5443f4f1c51
Bamb1to <stepangsavriluk@gmail.com>
2024-09-21 18:01:43 +0300

    what

--- Нова папка/window.py
+++ Нова папка/window.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit,QRadioButton, QHBoxLayout, QSpinBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit,QRadioButton, QHBoxLayout, QButtonGroup, QSpinBox, QGroupBox
+
 
 
 window = QWidget()# створення вікна
@@ -22,16 +23,53 @@ row1.addWidget(time_label)
 # Напис з запитанням
 question_text = QLabel("mouse")
 
-
-
-main_line = QVBoxLayout()
-main_line.addLayout(row1)
-main_line.addWidget(question_text, alignment=Qt.AlignCenter)
-
 btn1 = QRadioButton("ьірка")
-btn1 = QRadioButton("щур")
-btn1 = QRadioButton("мішок")
-btn1 = QRadioButton("миша")
+btn2 = QRadioButton("щур")
+btn3= QRadioButton("мішок")
+btn4 = QRadioButton("миша")
+
+radio_group = QButtonGroup()
+radio_group.addButton(btn1)
+radio_group.addButton(btn2)
+radio_group.addButton(btn3)
+radio_group.addButton(btn4)
+
+group_box = QGroupBox("Bаріанти перекладу")
+
+col1 = QVBoxLayout()
+col2 = QVBoxLayout()
+row2 = QHBoxLayout()
+
+col1.addWidget(btn1)
+col1.addWidget (btn2)
+
+col2.addWidget (btn3)
+col2.addWidget (btn4)
+
+row2.addLayout(col1)
+row2.addLayout(col2)
+
+group_box.setLayout(row2)
+
+result_box = QGroupBox("Результат")
+result_text = QLabel("Правильно")#Результат: Правильно/Неправильно
+answer_text = QLabel("миша")# Правильна відповідь
+
+result_col = QVBoxLayout()
+result_col.addWidget(result_text)
+result_col.addWidget(result_text, alignment=Qt.AlignCenter, stretch=2)
+
+result_box.setLayout(result_col)
+result_box.hide()
 
 answer_btn = QPushButton("Відповісти")
+
+# Додаємо всі віджети на головну напрямну лінію
+main_line = QVBoxLayout()
+main_line.addLayout(row1, stretch=1)
+main_line.addWidget(question_text, alignment=Qt.AlignCenter, stretch=2)
+main_line.addWidget(result_box, stretch=5)
+main_line.addWidget(group_box, stretch=5)
+main_line.addWidget(answer_btn, stretch=3)
+
 window.setLayout(main_line)
