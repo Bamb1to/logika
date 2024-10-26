1cd52c42ef9683a95141cbf393d1f080a48d7b0f
Bamb1to <stepangsavriluk@gmail.com>
2024-10-26 18:04:28 +0300

    wtf?

--- Генератор паролів/main.py
+++ Генератор паролів/main.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
-
+import secrets
+import string
 class Widget(QMainWindow):
     def   __init__(self):
         super().__init__()
@@ -11,7 +12,26 @@ class Widget(QMainWindow):
         self.ui.pushButton.clicked.connect(self.generate)
 
     def generate(self):
-        print("Тут буде ваш пароль")     
+        if self.ui.checkBox.isChecked() and self.ui.checkBox_2.isChecked():
+            symbols = string.ascii_letters + string.digits
+        elif self.ui.checkBox.isChecked():
+            symbols = string.digits
+        elif self.ui.checkBox_2.isChecked():
+            symbols = string.ascii_letters
+        else:
+            symbols = string.ascii_letters + string.digits
+
+        
+        
+        password = ""
+
+        for i in range(10):
+            password += secrets.choice(symbols)
+
+        self.ui.label_2.setText(password)
+
+        with open("passwords.txt", "a") as f:
+            f.write(password+"\n")
 
 app = QApplication([])
 ex = Widget()
